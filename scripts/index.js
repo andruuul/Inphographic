85b7fc6451e666121987c3fa82676ac0928cc34f
andruuul <andrescanulm@gmail.com>
2022-11-21 02:16:12 -0600

    finish prompts

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import { conversationPrompts } from "../utils/constants.js";
+
 const forms = document.querySelectorAll(".form");
 const addProfileButton = document.querySelector(".profile__add-button");
 console.log(addProfileButton);
@@ -9,3 +11,14 @@ const profiledata = [
     place: "",
   },
 ];
+
+//Prompts para conversaciones
+document.querySelector(".prompt__form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  showPrompt(document.querySelector(".prompt__form").prompt.value)
+})
+
+function showPrompt(promptNumber) {
+  document.querySelector(".prompt__category").textContent = conversationPrompts[promptNumber - 1].category
+  document.querySelector(".prompt__text").textContent = conversationPrompts[promptNumber - 1].text
+}
---
+++ utils/constants.js
@@ -0,0 +1,63 @@
+export const conversationPrompts = [
+  { category: "Hobbies", text: "What are your hobbies? Why do you like your hobbies so much? How often do you do these hobbies?  How long have you been doing these hobbies, and how did you get started?" },
+  { category: "Time", text: "How much free time do you usually have? How important is time to you? If you had more free time, what would you do? “Time is money.” Do you agree or disagree? Why?" },
+  { category: "Sleep", text: "How much sleep do you usually get? Why do some people sleep well while other people do not sleep well? What do you do when you have trouble sleeping? What time do you usually go to sleep? What time do you usually get up?" },
+  { category: "Music", text: "What types of music do you like/dislike? How do certain kinds of music make you feel? What types of music come from your country? What’s your favorite song/album/artist?" },
+  { category: "First Dates", text: "How many first dates have you had? How do you feel about first dates? What is a common first date like in your country? What is the best/worst first date experience you’ve ever had?" },
+  { category: "Work", text: "What work do/did you do? How do/did you like the work? What is your dream job? What work is common in your city/area/country?" },
+  { category: "Risk", text: `What is your definition of risk?
+  Are you a risk taker? Why/why not?
+  What are the advantages/disadvantages of taking risks?
+  What risks do you come across in your work/life?` },
+  { category: "Food", text: `What is your favorite food? Why?
+  What food comes from your country?
+  How do you feel when you eat food?
+  What foods do you dislike? Why?` },
+  { category: "Motivation", text: `How motivated are you in general?
+  What motivates you to do things?
+  What is the best motivator to succeed?
+  What do you do when you feel demotivated?` },
+  { category: "Beauty", text: `What is “beauty”?
+  What/who do you consider beautiful?
+  What does “inner beauty” mean to you?
+  Do you consider artificial beauty (cosmetic surgery) to still be beauty? Why/why not?` },
+  { category: "Crime", text: `Is crime a big problem in your city/country?
+  Have you ever been a victim of crime?
+  What crime is common in your city/country?
+  What would you do if you noticed a crime being committed?` },
+  { category: "Love", text: `What is love?
+  Who/what do you love?
+  What good/bad experiences have you had with love?
+  Can you be too young to be in love? Why/why not?` },
+  { category: "Goals", text: `What are your current goals in life?
+  How do you plan to reach your goals?
+  How often do you set goals for yourself?
+  What goals have you set and achieved in the past?` },
+  { category: "Dreams", text: `What kinds of dreams do you have?
+  What do you think dreams mean?
+  How much of your dreams do you remember? Why?
+  What is your opinion on premonitions? Are they real?` },
+  { category: "Restaurants", text: `How often do you go to restaurants?
+  What is your favorite restaurant? Why?
+  What do you usually order at a restaurant?
+  What is the restaurant experience like in your country?` },
+  { category: "Cooking", text: `In your home, who usually cooks?
+  How often do you cook?
+  How well do you cook? What can you cook well?
+  What are the advantages/disadvantages of cooking?` },
+  { category: "Money", text: `How well do you manage your money?
+  Why do some people have money problems?
+  What are some good ways to make money?
+  What would you do if I gave you $20/$2,000/$2,000,000?` },
+  { category: "Shopping", text: `Do you enjoy shopping? Why/why not?
+  What is your favorite shop? Why?
+  In your city, where is a good place to go shopping?` },
+  { category: "Plans", text: `How often do you plan things? Why?
+  What are your plans for (________)?
+  What are your plans for your English?
+  What do you think of this quote? “Having no plan is a plan to fail.”` },
+  { category: "Books", text: `Do you like books/reading? Why/why not?
+  What kind of books do/did you like?
+  What is your favorite book? Why?
+  What was the last book you read?` },
+]

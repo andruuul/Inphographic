87f09417ff7bcdbdd9274b3c932ccf14cb7879b0
andruuul <andrescanulm@gmail.com>
2022-11-21 21:11:33 -0600

    merge Profile component

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,27 @@
 //const forms = document.querySelectorAll(".form");
 //const addProfileButton = document.querySelector(".profile__add-button");
 //console.log(addProfileButton);
-const popup = document.querysellector(".popup");
-import { conversationPrompts } from "../utils/constants.js";
 
+import { conversationPrompts, profiledata } from "../utils/constants.js";
+import Popup from "../components/Popup.js";
+
+
+
+const popup = document.querySelector(".popup");
 const forms = document.querySelectorAll(".form");
 const addProfileButton = document.querySelector(".profile__add-button");
 console.log(addProfileButton);
 
-const profiledata = [
-  {
-    name: "Cristian Alegría",
-    url: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-    coment:
-      "Necesito practica un poco el ingles, tengo toda la tarde libre el miercoles. si alguien gusta practicar conmigo aqui esta un enlace para zoom: zoom.36546565#$ASdffdsghjfg$^&",
-  },
-  {
-    name: "Andrés Candul",
-    url: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-    coment:
-      "Necesito practica un poco el ingles, tengo toda la tarde libre el miercoles. si alguien gusta practicar conmigo aqui esta un enlace para zoom: zoom.36546565#$ASdffdsghjfg$^&",
-  },
-  {
-    name: "Aracely Cruz Del ángel",
-    url: "https://code.s3.yandex.net/web-code/lago.jpg",
-    coment:
-      "hi i want to practice english! send me an email! im usually free on Mondays aracely@gmail.com",
-  },
-];
 
+
+
+
+//////////////////////////Todo esto luego pasarlo a Profile.js - DESDE AQUÍ
 class Profile {
   constructor(data, selector) {
     this._name = data.name;
     this._photo = data.url;
-    this._coment = data.coment;
+    this._comment = data.comment;
     this._selector = selector;
   }
 
@@ -43,12 +31,26 @@ class Profile {
       .content.cloneNode(true);
     profileElement.querySelector(".avatar__photo").src = this._photo;
     profileElement.querySelector(".avatar__name").textContent = this._name;
-    profileElement.querySelector(".avatar-coment__text").textContent =
-      this._coment;
+    profileElement.querySelector(".avatar-comment__text").textContent = this._comment;
     console.log(profileElement);
     return profileElement;
   }
 }
+
+function handleNewprofileFormSubmit(evt) {
+  evt.preventDefault();
+  console.log("holi,voy aser un nuevo profile");
+}
+
+
+
+//////////////////////////Todo esto luego pasarlo a Profile.js - HASTA AQUÍ
+
+
+
+
+
+////ESTE SE VA A QUEDAR EN INDEX.JS
 
 profiledata.forEach((item) => {
   const profile = new Profile(item, ".template__profile");
@@ -57,9 +59,8 @@ profiledata.forEach((item) => {
   document.querySelector(".people__profiles").append(profileElement);
 });
 
-function handleNewprofileFormSubmit(evt) {
-  evt.preventDefault();
-  console.log("holi,voy aser un nuevo profile");
+
+
 //Prompts para conversaciones
 document.querySelector(".prompt__form").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -70,3 +71,11 @@ function showPrompt(promptNumber) {
   document.querySelector(".prompt__category").textContent = conversationPrompts[promptNumber - 1].category
   document.querySelector(".prompt__text").textContent = conversationPrompts[promptNumber - 1].text
 }
+
+const commentPopup = new Popup(".popup")
+commentPopup.setEventListeners()
+
+document.querySelector(".profile__add-button").addEventListener("click", (evt) => {
+  evt.preventDefault();
+  commentPopup.open()
+})

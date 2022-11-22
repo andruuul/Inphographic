4fb5ad3a41dd3007f7fe35bb1d2e6edcf5a7338e
andruuul <andrescanulm@gmail.com>
2022-11-22 08:46:07 -0600

    merge nav bar

--- scripts/nave-button.js
+++ scripts/nave-button.js
@@ -1,9 +1,8 @@
 const navButton = document.querySelector(".header__nave-button");
 const navBar = document.querySelector(".nav-bar");
 
-function togglenvBar(){
+function toggleNavBar(){
     navBar.classList.toggle("nav-bar__show");
-    navBar.classList.toggle("nav-bar__scale");
 }
 
-navButton.addEventListener("click", togglenvBar);
+navButton.addEventListener("click", toggleNavBar);
